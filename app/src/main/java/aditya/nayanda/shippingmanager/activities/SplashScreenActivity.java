03821bff84573caaf523ea2b85a251ce6ca2318f
nayandaCatch <nayanda.catch@gmail.com>
2018-03-25 15:44:10 +0700

    minor redesign <nayanda.haberty>

--- app/src/main/java/aditya/nayanda/shippingmanager/activities/SplashScreenActivity.java
+++ app/src/main/java/aditya/nayanda/shippingmanager/activities/SplashScreenActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
+import android.view.View;
 import android.widget.ImageView;
 
 import aditya.nayanda.shippingmanager.R;
@@ -23,8 +25,11 @@ public class SplashScreenActivity extends Activity {
         new Handler().postDelayed(() -> {
             Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
             ImageView logo = findViewById(R.id.app_logo);
+            ImageView pertaminaLogo = findViewById(R.id.pertamina_logo);
+            Pair<View, String> logoPair = new Pair<>(logo, "logo");
+            Pair<View, String> pertaminaLogoPair = new Pair<>(pertaminaLogo, "pertamina_logo");
             ActivityOptionsCompat options = ActivityOptionsCompat
-                    .makeSceneTransitionAnimation(SplashScreenActivity.this, logo, "logo");
+                    .makeSceneTransitionAnimation(SplashScreenActivity.this, pertaminaLogoPair, logoPair);
             startActivity(i, options.toBundle());
             finish();
         }, 1500);
