86f473825ed4a3514cd2f25a25c749c44b431b17
nayandaCatch <nayanda.catch@gmail.com>
2018-03-28 01:53:59 +0700

    added route calculator and push intent to google map <nayanda.haberty>

--- app/src/main/java/aditya/nayanda/shippingmanager/activities/MapsActivity.java
+++ app/src/main/java/aditya/nayanda/shippingmanager/activities/MapsActivity.java
@@ -1,8 +1,11 @@
 package aditya.nayanda.shippingmanager.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -27,10 +30,21 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import aditya.nayanda.shippingmanager.R;
 import aditya.nayanda.shippingmanager.activities.helper.ActivityHelper;
+import aditya.nayanda.shippingmanager.activities.helper.MapHelper;
 import aditya.nayanda.shippingmanager.fragments.dialog.JobDetailsDialogFragment;
 import aditya.nayanda.shippingmanager.model.Job;
 import aditya.nayanda.shippingmanager.model.Receiver;
@@ -40,88 +54,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private static boolean isPermissionGranted;
+    private final ArrayList<Job> jobs = new ArrayList<>();
     float DEFAULT_ZOOM = 14;
     private GoogleMap googleMap;
     private GeoDataClient geoDataClient;
     private PlaceDetectionClient placeDetectionClient;
     private FusedLocationProviderClient locationProviderClient;
-    private Job[] jobs;
     private Location lastKnownLocation;
     private Marker myMarker;
+    private ProgressDialog progressDialog;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView actionBarTitle = ActivityHelper.setToCustomActionBar(this);
-        actionBarTitle.setText(R.string.title_activity_maps);
-        setContentView(R.layout.activity_maps);
-        jobs = getJobsFromIntent();
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        geoDataClient = Places.getGeoDataClient(this, null);
-        placeDetectionClient = Places.getPlaceDetectionClient(this, null);
-        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-        findViewById(R.id.button_my_location).setOnClickListener(view -> getDeviceLocation());
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
-
-        setOnLongPressListener(googleMap, marker -> {
-            Job thisJob = (Job) marker.getTag();
-            if (thisJob == null) return;
-            FragmentManager fragmentManager = MapsActivity.this.getSupportFragmentManager();
-            JobDetailsDialogFragment dialogFragment = JobDetailsDialogFragment.newInstance(0.9f, thisJob);
-            dialogFragment.show(fragmentManager, "details_dialog");
-        });
-
-        googleMap.setOnMarkerClickListener(marker -> {
-            if (marker.getTag() != null) {
-                Toast toast = Toast.makeText(getApplicationContext(), R.string.toast_long_press_info, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM, 0, 342);
-                toast.show();
-            }
-            return false;
-        });
-
-        getLocationPermission();
-        if (isPermissionGranted) getDeviceLocation();
-        if (jobs == null) jobs = getJobsFromIntent();
-        for (Job job : jobs) {
-            addMarker(job);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        Intent mainIntent = new Intent(this.getApplicationContext(), MainActivity.class);
-        mainIntent.putExtra("JOBS", jobs);
-        mainIntent.putExtra("INDEX", 0);
-        startActivity(mainIntent);
-        finish();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        isPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    isPermissionGranted = true;
-                }
-            }
-        }
-        updateLocationUI();
-    }
-
-    private void setOnLongPressListener(GoogleMap googleMap, OnLongPressListener onLongPressListener) {
+    private static void setOnLongPressListener(GoogleMap googleMap, OnLongPressListener onLongPressListener) {
 
         //Simulate long click
         googleMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
@@ -149,6 +92,95 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView actionBarTitle = ActivityHelper.setToCustomActionBar(this);
+        actionBarTitle.setText(R.string.title_activity_maps);
+
+        setContentView(R.layout.activity_maps);
+        Collections.addAll(jobs, getJobsFromIntent());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        geoDataClient = Places.getGeoDataClient(this, null);
+        placeDetectionClient = Places.getPlaceDetectionClient(this, null);
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        findViewById(R.id.button_my_location).setOnClickListener(view -> getDeviceLocation());
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        setMarkerListener(googleMap);
+
+        if (jobs.size() == 0) {
+            Collections.addAll(jobs, getJobsFromIntent());
+        }
+        for (Job job : jobs) {
+            addMarker(job);
+        }
+        if (jobs.size() > 0) {
+            findViewById(R.id.button_start_navigation).setOnClickListener(view -> {
+                Uri gMapIntentUri = MapHelper.createGoogleMapRouteIntentUri(jobs.get(0).getLocation());
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gMapIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            });
+        }
+        getLocationPermission();
+        if (isPermissionGranted) getDeviceLocation();
+
+    }
+
+    private void setMarkerListener(GoogleMap googleMap) {
+        setOnLongPressListener(googleMap, marker -> {
+            Job thisJob = (Job) marker.getTag();
+            if (thisJob == null) return;
+            FragmentManager fragmentManager = MapsActivity.this.getSupportFragmentManager();
+            JobDetailsDialogFragment dialogFragment = JobDetailsDialogFragment.newInstance(0.9f, thisJob);
+            dialogFragment.show(fragmentManager, "details_dialog");
+        });
+
+        googleMap.setOnMarkerClickListener(marker -> {
+            if (marker.getTag() != null) {
+                Toast toast = Toast.makeText(getApplicationContext(), R.string.toast_long_press_info, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.BOTTOM, 0, 342);
+                toast.show();
+            }
+            return false;
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent mainIntent = new Intent(this.getApplicationContext(), MainActivity.class);
+        mainIntent.putExtra("JOBS", jobs);
+        mainIntent.putExtra("INDEX", 0);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        isPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    isPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
     }
 
     private void getLocationPermission() {
@@ -202,11 +234,24 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         } else {
                             myMarker.setPosition(latLng);
                         }
+                        calculateRoute(latLng, jobs);
                     }
                 });
             }
         } catch (SecurityException e) {
             Log.e("ERROR", e.toString());
+        }
+    }
+
+    private void calculateRoute(LatLng origin, List<Job> jobs) {
+        if (jobs.size() > 0) {
+            ArrayList<LatLng> params = new ArrayList<>();
+            params.add(origin);
+            for (Job job : jobs) {
+                params.add(job.getLocation());
+            }
+            new RouteRequester(MapHelper.RouteOrder.OPTIMIZE, this.getString(R.string.google_maps_key))
+                    .execute(params.toArray(new LatLng[params.size()]));
         }
     }
 
@@ -233,5 +278,87 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private interface OnLongPressListener {
         void onLongPress(Marker marker);
+    }
+
+    private class RouteRequester extends AsyncTask<LatLng, Integer, List<LatLng>> {
+
+        private final MapHelper.RouteOrder order;
+        private final String apiKey;
+
+        RouteRequester(MapHelper.RouteOrder order, String apiKey) {
+            this.order = order;
+            this.apiKey = apiKey;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MapsActivity.this);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressDialog.setTitle(R.string.title_calculating_route);
+            progressDialog.setMessage(getString(R.string.message_calculating_route));
+            progressDialog.setIndeterminate(false);
+            progressDialog.show();
+            publishProgress(0);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            if (progressDialog != null) {
+                progressDialog.setProgress(values[0]);
+            }
+        }
+
+        @Override
+        protected List<LatLng> doInBackground(LatLng... latLngs) {
+            publishProgress(15);
+            LatLng origin = latLngs[0];
+            List<LatLng> destinations = Arrays.asList(latLngs).subList(1, latLngs.length);
+            publishProgress(30);
+            try {
+                JSONObject result = MapHelper.requestRoute(origin, destinations, order, apiKey);
+                publishProgress(55);
+                List<LatLng> polyLine = MapHelper.parseJsonRoute(result);
+                List<Integer> routeOrder = MapHelper.getWayPointsOrder(result);
+                publishProgress(70);
+                if (routeOrder.size() == jobs.size()) {
+                    List<Job> jobsClone = new ArrayList<>();
+                    jobsClone.addAll(jobs);
+                    jobs.clear();
+                    publishProgress(85);
+                    for (Integer order : routeOrder) {
+                        jobs.add(jobsClone.get(order));
+                    }
+                }
+                publishProgress(100);
+                return polyLine;
+            } catch (IOException | JSONException e) {
+                Log.e("REQUEST ERROR", e.toString());
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<LatLng> result) {
+            if (result != null) {
+                if (result.size() > 2) {
+                    PolylineOptions lineOptions;
+                    lineOptions = new PolylineOptions();
+                    lineOptions.addAll(result);
+                    lineOptions.width(15);
+                    lineOptions.color(getColor(R.color.colorPrimary));
+                    MapsActivity.this.googleMap.addPolyline(lineOptions);
+                } else showFailedGetRouteMessage();
+            }
+            if (result == null) showFailedGetRouteMessage();
+            if (progressDialog != null) progressDialog.dismiss();
+        }
+
+        private void showFailedGetRouteMessage() {
+            Toast toast = Toast.makeText(getApplicationContext(), R.string.message_failed_get_route, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
     }
 }
